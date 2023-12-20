79517bd79913f3334cb1487d4277533180c61915
Paul Onawola <onawola.ire@gmail.com>
2023-12-20 11:45:35 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Paul Onawola on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
